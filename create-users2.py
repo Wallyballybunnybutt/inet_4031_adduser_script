24a1cf843dd5ba7d3bc3bb40d2c6d5375a806244
Amanda Mathison <amathisons@yahoo.com>
2026-03-19 03:17:40 +0000

    Update create-users2.py and other files

--- create-users2.py
+++ create-users2.py
@@ -13,11 +13,13 @@ import re
 import sys
 
 def main():
+#Asks the user is they want to do a dry run
+    dry_run = input("Are you doing a dry run?(Y/N): ").strip().upper() == "Y"
     for line in sys.stdin:
 
         #Checks if the line starts with '#' which would be a command
         #It skips comments or non data lines in the input file
-        match = re.match("^#",line)
+        match = re.match("^#", line)
 
         #Removes the white space and splits the line with ':'
 	#Each line is supposed to contain 5 fields/categories
@@ -25,7 +27,12 @@ def main():
 
         #skips if the line doesn't have 5 fields
 	#this code will only accept records that are properly formatted with 5 fields
-        if match or len(fields) != 5:
+        if len(fields) != 5:
+            if dry_run:
+                if match: #skips line if #
+                    print("DRY RUN so skipping comment line:", line.strip())
+            else: #skips if wrong number of fields
+                print("DRY RUN so skipping lines with wrong number of fields:", line.strip())
             continue
         #Takes the user information from the specific fields
 	#username: user's username
@@ -33,6 +40,7 @@ def main():
 	#gecos: properly formatts the user's information
         username = fields[0]
         password = fields[1]
+        groups = fields[4].split(',')
         gecos = "%s %s,,," % (fields[3],fields[2])
 
         #splits the group fields so that the user can be assigned to multiple groups
@@ -44,22 +52,26 @@ def main():
 	#builds system command to create the user account with disabled password and specified user info
 	#
         cmd = "/usr/sbin/adduser --disabled-password --gecos '%s' %s" % (gecos,username)
-
+        if dry_run: #if in dry run just print the command instead of running it
+            print("DRY RUN: Would run:", cmd) #
+        else: # runs the command to create the user
+            os.system(cmd)
         #keeps these commented to verify commands before execution
         #if it's uncommented it will execute the command and create the user account
         #print(cmd)
-        os.system(cmd)
 
         #Displays a message indicating that the password is set
         print("==> Setting the password for %s..." % (username))
 	#Creates the command to set the user's password
 	#uses echo to send the password more than once
         cmd = "/bin/echo -ne '%s\n%s' | /usr/bin/sudo /usr/bin/passwd %s" % (password,password,username)
-
+        if dry_run: #prints the command that would set the password
+            print("DRY RUN: Would run:", cmd)
+        else: #runs the command to set the password
+            os.system(cmd)
         #Keeps commented to verify if it's correct
         #If it's uncommented it will set the user's password
         #print(cmd)
-        os.system(cmd)
 
         for group in groups:
             #Checks if the group is not '-' which means their isn't a group assignment
@@ -68,6 +80,9 @@ def main():
                 print("==> Assigning %s to the %s group..." % (username,group))
                 cmd = "/usr/sbin/adduser %s %s" % (username,group)
                 #print(cmd)
+            if dry_run: #dry runs prints the command instead of running it
+                print("DRY RUN: Would run:", cmd)
+            else: #runs the command to add the user to the group
                 os.system(cmd)
 
 if __name__ == '__main__':
